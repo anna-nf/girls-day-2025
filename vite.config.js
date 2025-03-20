d094e9d6c63d0abc3e7fde239b1ca61e698d009b
anna-nf <akazulak@netfonds.de>
2025-03-20 17:40:52 +0100

    Fix: Include dist folder in repository

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/girls-day-2025/',
-
+  base: '/netfonds-gruppe-girls-day-2025/', 
   resolve: {
     alias: {
       vue: 'vue/dist/vue.esm-bundler.js'
@@ -14,7 +12,6 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     rollupOptions: {
-      external: [],
       output: {
         manualChunks: undefined
       }
