813c175e37380b9f7faf2321c8e329cf27fc212a
anna-nf <akazulak@netfonds.de>
2025-03-20 15:42:32 +0100

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' 
-    ? '/netfonds-gruppe-girls-day-2025/' 
-    : '/',
+  base: '/girls-day-2025/',
+
   resolve: {
     alias: {
       vue: 'vue/dist/vue.esm-bundler.js'
