8c572261a76cfee2491aac09284428cd0f962d5c
anna-nf <akazulak@netfonds.de>
2025-03-21 13:06:23 +0100

    Fix: Add bottom navigation to App component

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,20 @@
     <v-main>
       <router-view />
     </v-main>
+    
+    <v-bottom-navigation name="bottom-navigation">
+      <!--  navigation items here -->
+      <v-btn value="home">
+        <v-icon>mdi-home</v-icon>
+        Home
+      </v-btn>
+      <v-btn value="about">
+        <v-icon>mdi-information</v-icon>
+        About
+      </v-btn>
+      <!--  buttons  -->
+    </v-bottom-navigation>
+    
     <AppFooter />
   </v-app>
 </template>
